d105939551d359e17621bae98c8ad4cad5036870
DNKpp <DNKpp2011@gmail.com>
2020-07-27 10:25:15 +0200

    add Defines.hpp; add EntityState enum
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

---
+++ include/Simple-ECS/Defines.hpp
@@ -0,0 +1,22 @@
+
+//          Copyright Dominic Koepke 2020 - 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef SECS_DEFINES_HPP
+#define SECS_DEFINES_HPP
+
+#pragma once
+
+namespace secs
+{
+	enum class EntityState
+	{
+		initializing,
+		running,
+		tearDown
+	};
+}
+
+#endif
