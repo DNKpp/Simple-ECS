482401f77395d6f9fed1c6c4013d127c3ac21921
DNKpp <DNKpp2011@gmail.com>
2020-09-02 19:01:53 +0200

    add destructible requirement to Component and System concepts
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-ECS/Concepts.hpp
+++ include/Simple-ECS/Concepts.hpp
@@ -1,4 +1,3 @@
-
 //          Copyright Dominic Koepke 2020 - 2020.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
@@ -15,15 +14,16 @@
 namespace secs
 {
 	template <class T>
-	concept Component = std::movable<T>;
+	concept Component = std::movable<T> && std::destructible<T>;
 
 	template <class T>
-	concept System = std::move_constructible<T> && requires (T system)
+	concept System = std::move_constructible<T> &&
+	std::destructible<T> &&
+	requires(T system)
 	{
 		typename T::ComponentType;
-		typename T::ComponentHandle;
-	} && Component<typename T::ComponentType>;
-
+	} &&
+	Component<typename T::ComponentType>;
 }
 
 #endif
