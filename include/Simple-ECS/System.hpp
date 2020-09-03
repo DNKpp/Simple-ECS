7c95637b456fd2f91b44340a0f39ea51bdae275f
DNKpp <DNKpp2011@gmail.com>
2020-09-03 22:58:58 +0200

    add System.hpp documentation
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-ECS/System.hpp
+++ include/Simple-ECS/System.hpp
@@ -38,6 +38,10 @@ namespace secs
 		}
 	};
 
+	
+	/**
+	 * \brief Interface for System classes
+	 */
 	class ISystem
 	{
 	public:
@@ -135,11 +139,21 @@ namespace secs::detail
 
 namespace secs
 {
+	/**
+	 * \brief Base class for custom Systems
+	 *
+	 * This is the class you should inherit from, when you are about to create a custom System for a corresponding Component type.
+	 * There are some virtual member functions you could override to tweak the behaviour of your Systems.
+	 * Each System type should only instantiated once during the runtime of your program.
+	 * \tparam TComponent The associated Component type.
+	 */
 	template <class TComponent>
+	// ReSharper disable once CppClassCanBeFinal
 	class SystemBase :
 		public ISystem
 	{
 	private:
+		friend class World;
 		friend struct detail::ComponentRtti;
 
 		struct ComponentInfo
@@ -149,15 +163,175 @@ namespace secs
 		};
 
 	public:
+		/**
+		 * \brief Alias for the associated Component type.
+		 */
 		using ComponentType = TComponent;
 
 		SystemBase(const SystemBase&) = delete;
 		SystemBase& operator =(const SystemBase&) = delete;
 
+		/**
+		 * \brief Move constructor
+		 */
 		SystemBase(SystemBase&&) /* NOT noexcept*/ = default;
+		
+		/**
+		 * \brief Move assignment
+		 */
 		SystemBase& operator =(SystemBase&&) /* NOT noexcept*/ = default;
 
+		
+		/**
+		 * \brief Destructor
+		 */
 		~SystemBase() noexcept override = default;
+
+		/**
+		 * \brief Checks if Component is present and active
+		 * \param uid Uid of the Component object.
+		 * \return True if Component object is present and active.
+		 */
+		[[nodiscard]] constexpr bool hasComponent(Uid uid) const noexcept
+		{
+			return 0u < uid && uid <= std::size(m_Components) && m_Components[uid - 1u];
+		}
+
+		/**
+		 * \brief Queries for a Component object
+		 * \param uid Uid of the Component object.
+		 * \return Const pointer to the stored Component object or nullptr if not valid.
+		 */
+		[[nodiscard]] constexpr const TComponent* findComponent(Uid uid) const noexcept
+		{
+			if (0u < uid && uid <= std::size(m_Components))
+			{
+				if (auto& info = m_Components[uid - 1u])
+					return &info->component;
+			}
+			return nullptr;
+		}
+
+		/**
+		 * \brief Queries for a Component object
+		 * \param uid Uid of the Component object.
+		 * \return pointer to the stored Component object or nullptr if not valid.
+		 */
+		[[nodiscard]] constexpr TComponent* findComponent(Uid uid) noexcept
+		{
+			return const_cast<TComponent*>(std::as_const(*this).findComponent(uid));
+		}
+
+		/**
+		 * \brief Queries for a Component object
+		 * \param uid Uid of the Component object.
+		 * \throws SystemError if the Component object at uid is not valid.
+		 * \return Const reference to the stored Component object.
+		 */
+		[[nodiscard]] constexpr const TComponent& component(Uid uid) const
+		{
+			if (hasComponent(uid))
+			{
+				return m_Components[uid - 1u]->component;
+			}
+			using namespace std::string_literals;
+			throw SystemError("System: \""s + typeid(*this).name() + "\" Component uid: " + std::to_string(uid) + " not found.");
+		}
+
+		/**
+		 * \brief Queries for a Component object
+		 * \param uid Uid of the Component object.
+		 * \throws SystemError if the Component object at uid is not valid.
+		 * \return Reference to the stored Component object.
+		 */
+		[[nodiscard]] constexpr TComponent& component(Uid uid)
+		{
+			return const_cast<TComponent&>(std::as_const(*this).component(uid));
+		}
+
+		
+		/**
+		 * \brief Counts active Components
+		 * \return Amount of active Component objects.
+		 */
+		[[nodiscard]] constexpr std::size_t size() const noexcept
+		{
+			return m_ComponentCount;
+		}
+
+		/**
+		 * \brief Empty
+		 * \return True if no active Component objects present.
+		 */
+		[[nodiscard]] constexpr bool empty() const noexcept
+		{
+			return m_ComponentCount == 0;
+		}
+
+		/**
+		 * \brief preUpdate
+		 *
+		 * This function may be overriden to conduct necessary preparations for the next update call.
+		 */
+		void preUpdate() override
+		{
+		}
+
+		/**
+		 * \brief update
+		 *
+		 * This function may be overriden to perform frequent actions on components.
+		 * \param delta Time elapsed until previous update call.
+		 */
+		void update(float delta) override
+		{
+		}
+
+		/**
+		 * \brief postUpdate
+		 *
+		 * This function may be overriden to conduct necessary finalization steps for the latest update call.
+		 */
+		void postUpdate() override
+		{
+		}
+
+	protected:
+		/**
+		 * \brief Protected default Constructor
+		 */
+		SystemBase() = default;
+
+		/**
+		 * \brief Entity state changed
+		 *
+		 * This function will be called when an Component associated Entity changed its state. It may be overridden.
+		 */
+		virtual void derivedEntityStateChanged(TComponent& component, Entity& entity)
+		{
+		}
+
+		/**
+		 * \brief Executes action on each active Component
+		 * \tparam TComponentAction Invokable object with specific signature.
+		 * \param action Invokable object.
+		 */
+		template <std::invocable<Entity&, TComponent&> TComponentAction>
+		void forEachComponent(TComponentAction action)
+		{
+			for (auto& info : m_Components)
+			{
+				if (info)
+				{
+					assert(info->entity);
+					action(*info->entity, info->component);
+				}
+			}
+		}
+
+	private:
+		std::size_t m_ComponentCount = 0;
+		std::deque<std::optional<ComponentInfo>> m_Components;
 
 		template <class TComponentCreator = utils::EmptyCallable<TComponent>>
 		[[nodiscard]] Uid createComponent(TComponentCreator&& creator = TComponentCreator{})
@@ -173,87 +347,6 @@ namespace secs
 			++m_ComponentCount;
 			return static_cast<Uid>(std::size(m_Components));
 		}
-
-		[[nodiscard]] constexpr bool hasComponent(Uid uid) const noexcept
-		{
-			return 0u < uid && uid <= std::size(m_Components) && m_Components[uid - 1u];
-		}
-
-		[[nodiscard]] constexpr const TComponent* findComponent(Uid uid) const noexcept
-		{
-			if (0u < uid && uid <= std::size(m_Components))
-			{
-				if (auto& info = m_Components[uid - 1u])
-					return &info->component;
-			}
-			return nullptr;
-		}
-
-		[[nodiscard]] constexpr TComponent* findComponent(Uid uid) noexcept
-		{
-			return const_cast<TComponent*>(std::as_const(*this).findComponent(uid));
-		}
-
-		[[nodiscard]] constexpr const TComponent& component(Uid uid) const
-		{
-			if (hasComponent(uid))
-			{
-				return m_Components[uid - 1u]->component;
-			}
-			using namespace std::string_literals;
-			throw SystemError("System: \""s + typeid(*this).name() + "\" Component uid: " + std::to_string(uid) + " not found.");
-		}
-
-		[[nodiscard]] constexpr TComponent& component(Uid uid)
-		{
-			return const_cast<TComponent&>(std::as_const(*this).component(uid));
-		}
-
-		[[nodiscard]] constexpr std::size_t size() const noexcept
-		{
-			return m_ComponentCount;
-		}
-
-		[[nodiscard]] constexpr bool empty() const noexcept
-		{
-			return m_ComponentCount == 0;
-		}
-
-		template <class TComponentAction = utils::EmptyCallable<>>
-		void forEachComponent(TComponentAction&& action = TComponentAction())
-		{
-			for (auto& info : m_Components)
-			{
-				if (info)
-				{
-					assert(info->entity);
-					action(*info->entity, info->component);
-				}
-			}
-		}
-
-		void preUpdate() override
-		{
-		}
-
-		void update(float delta) override
-		{
-		}
-
-		void postUpdate() override
-		{
-		}
-
-	protected:
-		SystemBase() = default;
-
-		virtual void derivedEntityStateChanged(TComponent& component, Entity& entity)
-		{
-		}
-
-	private:
-		std::size_t m_ComponentCount = 0;
-		std::deque<std::optional<ComponentInfo>> m_Components;
 
 		void setComponentEntity(Uid uid, Entity& entity) noexcept
 		{
