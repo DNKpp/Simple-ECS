1be1b9d4cd735d3049f02bd8b0f0b226ccc73c47
DNKpp <DNKpp2011@gmail.com>
2020-09-03 22:08:35 +0200

    add documentation for Concepts.hpp
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-ECS/Concepts.hpp
+++ include/Simple-ECS/Concepts.hpp
@@ -13,9 +13,25 @@
 
 namespace secs
 {
+	/** \defgroup concepts Concepts 
+	 */
+
+	/** \var Component
+	 * \brief Concept for Component types.
+	 *
+	 * Requires Component objects to be movable and destructible.
+	 * \ingroup concepts
+	 */
 	template <class T>
 	concept Component = std::movable<T> && std::destructible<T>;
 
+	/** \var System
+	 * \brief Concept for System types.
+	 *
+	 * Requires System objects to be move constructable, destructible and expose their corresponding Component type
+	 * as CompponentType.
+	 * \ingroup concepts
+	 */
 	template <class T>
 	concept System = std::move_constructible<T> &&
 	std::destructible<T> &&
