62a333764af6dd9ec900183d55ccd5abb7568bef
DNKpp <DNKpp2011@gmail.com>
2020-09-03 22:31:10 +0200

    add Entity documentation
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- include/Simple-ECS/Entity.hpp
+++ include/Simple-ECS/Entity.hpp
@@ -41,16 +41,29 @@ namespace secs
 		}
 	};
 
+	/**
+	 * \brief RAII wrapper for a collection of Components
+	 *
+	 * This class actually acts as a RAII wrapper for its associated Components. During Construction it receives handle like infos
+	 * for its "owning" Components and will destruct them during its own destruction. Each Entity has its own unique identifier (uid) which
+	 * will never reused during a programs session. It is safe to store references or pointers to a specific Entity object, because an Entity
+	 * will never be moved around in memory during its lifetime.
+	 */
 	class Entity
 	{
 		friend class World;
-		
+
 	public:
 		Entity(const Entity&) = delete;
 		Entity& operator =(const Entity&) = delete;
 		Entity(Entity&&) = delete;
 		Entity& operator =(Entity&&) = delete;
 
+		/**
+		 * \brief Constructor
+		 * \param uid Unique identifier for this Entity
+		 * \param componentInfos Infos for the Components this Entity will be responsible for.
+		 */
 		explicit Entity(Uid uid, std::vector<detail::ComponentStorageInfo> componentInfos) :
 			m_Uid{ uid },
 			m_ComponentInfos{ std::move(componentInfos) }
@@ -59,6 +72,9 @@ namespace secs
 			setComponentEntity();
 		}
 
+		/**
+		 * \brief Destructor
+		 */
 		~Entity() noexcept
 		{
 			for (auto& info : m_ComponentInfos)
@@ -68,22 +84,45 @@ namespace secs
 			}
 		}
 
+		/**
+		 * \brief Unique identifier
+		 * \return Returns the uid of this Entity.
+		 */
 		[[nodiscard]] constexpr Uid uid() const noexcept
 		{
 			return m_Uid;
 		}
 
+		/**
+		 * \brief Current state
+		 * \return Returns the current \ref EntityState of this Entity.
+		 */
 		[[nodiscard]] constexpr EntityState state() const noexcept
 		{
 			return m_State;
 		}
 
+		/**
+		 * \brief Checks if Component is present
+		 *
+		 * \remark This function does not perform any inheritance checks, thus you can always query for concrete Component types.
+		 * \tparam TComponent Expected Component type.
+		 * \return True if Entity has Component of type TComponent.
+		 */
 		template <Component TComponent>
 		[[nodiscard]] bool hasComponent() const noexcept
 		{
 			return findComponentInfo<TComponent>(m_ComponentInfos) != end(m_ComponentInfos);
 		}
 
+		/**
+		 * \brief Queries for a specific Component type
+		 *
+		 * This function searches for a specific Component type and returns a const pointer to the caller.
+		  * \remark This function does not perform any inheritance checks, thus you can always query for concrete Component types.
+		 * \tparam TComponent Expected Component type.
+		 * \return Const pointer to the stored Component object or nullptr if not found.
+		 */
 		template <Component TComponent>
 		[[nodiscard]] const TComponent* findComponent() const noexcept
 		{
@@ -95,12 +134,29 @@ namespace secs
 			return nullptr;
 		}
 
+		/**
+		 * \brief Queries for a specific Component type
+		 *
+		 * This function searches for a specific Component type and returns a pointer to the caller.
+		  * \remark This function does not perform any inheritance checks, thus you can always query for concrete Component types.
+		 * \tparam TComponent Expected Component type.
+		 * \return Pointer to the stored Component object or nullptr if not found.
+		 */
 		template <Component TComponent>
 		[[nodiscard]] TComponent* findComponent() noexcept
 		{
 			return const_cast<TComponent*>(std::as_const(*this).findComponent<TComponent>());
 		}
 
+		/**
+		 * \brief Queries for a specific Component type
+		 *
+		 * This function searches for a specific Component type and returns a const reference to the caller.
+		  * \remark This function does not perform any inheritance checks, thus you can always query for concrete Component types.
+		 * \tparam TComponent Expected Component type.
+		 * \throws EntityError if a Component object could not be found.
+		 * \return Const reference to the stored Component object.
+		 */
 		template <Component TComponent>
 		[[nodiscard]] const TComponent& component() const
 		{
@@ -112,6 +168,15 @@ namespace secs
 			throw EntityError("Component not found: "s + typeid(TComponent).name());
 		}
 
+		/**
+		 * \brief Queries for a specific Component type
+		 *
+		 * This function searches for a specific Component type and returns a reference to the caller.
+		 * \remark This function does not perform any inheritance checks, thus you can always query for concrete Component types.
+		 * \tparam TComponent Expected Component type.
+		 * \throws EntityError if a Component object could not be found.
+		 * \return Reference to the stored Component object.
+		 */
 		template <Component TComponent>
 		[[nodiscard]] TComponent& component()
 		{
