8309cd12be9dbdce9d918e341938fe5c8a013af1
DNKpp <DNKpp2011@gmail.com>
2020-09-03 23:17:41 +0200

    fix year
    Signed-off-by: DNKpp <DNKpp2011@gmail.com>

--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,4 @@
-//          Copyright Dominic Koepke 2017 - 2020.
+//          Copyright Dominic Koepke 2020 - 2020.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          https://www.boost.org/LICENSE_1_0.txt)
